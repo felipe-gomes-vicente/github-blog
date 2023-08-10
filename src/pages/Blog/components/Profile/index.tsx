727902701de18e61e46e5953727ce4d41281100f
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-08-10 11:34:36 -0300

    fix DefaultLayout and created pages/Blog/component/Profile

---
+++ src/pages/Blog/components/Profile/index.tsx
@@ -0,0 +1,9 @@
+import { ProfileContainer, ProfilePicture } from './styles'
+
+export function Profile() {
+  return (
+    <ProfileContainer>
+      <ProfilePicture src="https://github.com/diego3g.png" />
+    </ProfileContainer>
+  )
+}
