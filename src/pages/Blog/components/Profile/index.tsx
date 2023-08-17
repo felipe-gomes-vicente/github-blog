4971c93e8ebff88e5441c6ef91a6c2281c7e6a1a
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-08-17 10:35:34 -0300

    dynamic pages/Blog/components/Profile, created interface ProfileData, states profileData and isLoading, created function getProfileData

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,37 +1,72 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { useCallback, useEffect, useState } from 'react'
 
+import { api } from '../../../../lib/axios'
 import { ExternalLink } from '../../../../components/ExternalLink'
 
 import { ProfileContainer, ProfileDetails, ProfilePicture } from './styles'
 
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
+interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  html_url: string
+  name: string
+  company?: string
+  followers: number
+}
+
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/users/${username}`)
+
+      setProfileData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [profileData])
+
+  useEffect(() => {
+    getProfileData()
+  }, [])
+
   return (
     <ProfileContainer>
-      <ProfilePicture src="https://github.com/felipe-gomes-vicente.png" />
+      <ProfilePicture src={profileData.avatar_url} />
 
       <ProfileDetails>
         <header>
-          Felipe Vicente
-          <ExternalLink text="Github" />
+          {profileData.name}
+          <ExternalLink
+            text="Github"
+            href={profileData.html_url}
+            target="_blank"
+          />
         </header>
-        <p>
-          Software Engineer | Front End | React, React Native, Next.js,
-          Typescript, and Node.js
-        </p>
+        <p>{profileData.bio}</p>
         <ul>
           <li>
             <FontAwesomeIcon icon={faGithub} />
-            felipe-gomes-vicente
+            {profileData.login}
           </li>
-          <li>
-            <FontAwesomeIcon icon={faBuilding} />
-            Cidade Curitiba
-          </li>
+          {profileData?.company && (
+            <li>
+              <FontAwesomeIcon icon={faBuilding} />
+              {profileData.company}
+            </li>
+          )}
           <li>
             <FontAwesomeIcon icon={faUserGroup} />
-            123 seguidores
+            {profileData.followers}
           </li>
         </ul>
       </ProfileDetails>
